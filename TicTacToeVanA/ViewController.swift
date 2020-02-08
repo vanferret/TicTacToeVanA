34875d610618ab746c1f82b3de6bf2efafd6fbe0
student <student@mbp-stud-10.local>
2020-02-07 22:30:20 -0600

    Initial Commit

---
+++ TicTacToeVanA/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TicTacToeVanA
+//
+//  Created by student on 2/7/20.
+//  Copyright © 2020 vanderson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
